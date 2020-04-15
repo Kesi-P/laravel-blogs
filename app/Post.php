fb1c52b3224834945d012ce2ba0f4a2cdc3ae165
Kesi Promnual <kesi.promnual@myzone.org>
2020-04-15 12:10:01 +0100

    Many to Many
    Tag and Post relationship
    create a post_tag table

--- app/Post.php
+++ app/Post.php
@@ -24,4 +24,11 @@ class Post extends Model
    {
      return $this->beongsTo('App\Category');
    }
+   //tags->tags be a sigular
+   //posts->post p before t
+   //create a new table post_tag
+   public function tags()
+   {
+     return $this->belongsToMany('App\Tag');
+   }
 }
